5b3835b3f027be09ea8ec11188c987d88c1f80fb
Marcos Santos serrano <msantos-@c2r15s3.42madrid.com>
2020-07-03 12:46:50 +0200

    main

--- src/main.c
+++ src/main.c
@@ -6,13 +6,14 @@
 /*   By: msantos- <msantos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 20:22:49 by msantos-          #+#    #+#             */
-/*   Updated: 2020/05/26 21:41:38 by msantos-         ###   ########.fr       */
+/*   Updated: 2020/07/02 12:41:55 by msantos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "../minilibx_opengl/mlx.h"
 #include "cub3d.h"
+#include "map1.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -21,22 +22,79 @@
 
 int deal_key(int key, t_cub3d *f)
 {
+	/*system("clear");*/
+	
 	ft_putstr(ft_itoa(key));
+	ft_putstr("\n");
 	
 	if (key == 123)
-		f->posX = f->posX - 14;
+		f->x_position = f->x_position - 15;
 	else if (key == 124)
-		f->posX = f->posX + 14;
+		f->x_position = f->x_position + 15;
 	else if (key == 126)
-		f->posY = f->posY - 14;
+		f->y_position = f->y_position - 15;
 	else if (key == 125)
-		f->posY = f->posY + 14;
+		f->y_position = f->y_position + 15;
 	else if (key == 53)
 		system("killall a.out && clear"); //system("kill -9 | ps -ef | pgrep a.out");
 
-	mlx_string_put(f->mlx_ptr, f->win_ptr, f->x_position, f->y_position, 0x27FF00, "*");
+	ft_putstr(ft_itoa(f->x_position));
+	//mlx_string_put(f->mlx_ptr, f->win_ptr, f->x_position, f->y_position, 0x27FF00, "*");
 	//mlx_pixel_put(f->mlx_ptr, f->win_ptr, f->x_position, f->y_position, 0x27FF00);
+	mlx_pixel_put(f->mlx_ptr, f->win_ptr, f->x_position, f->y_position, 0x27FF00);
+	mlx_pixel_put(f->mlx_ptr, f->win_ptr, f->x_position + 1, f->y_position + 1, 0x27FF00);
+	mlx_pixel_put(f->mlx_ptr, f->win_ptr, f->x_position - 1, f->y_position - 1, 0x27FF00);
+	mlx_pixel_put(f->mlx_ptr, f->win_ptr, f->x_position - 1, f->y_position + 1, 0x27FF00);
+	mlx_pixel_put(f->mlx_ptr, f->win_ptr, f->x_position + 1, f->y_position - 1, 0x27FF00);
 	return (0);
+}
+
+void drawMap(t_cub3d *f)
+{
+	int y;
+	int x;
+
+	int x_wall;
+	int y_wall;
+	
+	x = 0;
+	y = 0;
+
+	x_wall = 15;
+	y_wall = 15;
+
+	while (y < mapHeight)
+	{
+		
+		while (x < mapWidth)
+		{
+			/*printf("Casillas: %i",x);*/
+			if (worldMap[x][y] == 1 || worldMap[x][y] == 2 || worldMap[x][y] == 3 || worldMap[x][y] == 4 || worldMap[x][y] == 5)
+			{
+				/*mlx_string_put(f->mlx_ptr, f->win_ptr, x_wall, y_wall, 0xFA2C00, "*");*/
+				
+				mlx_pixel_put(f->mlx_ptr, f->win_ptr, x_wall, y_wall, 0xFA2C00);
+				mlx_pixel_put(f->mlx_ptr, f->win_ptr, x_wall+1, y_wall+1, 0xFA2C00);
+				mlx_pixel_put(f->mlx_ptr, f->win_ptr, x_wall-1, y_wall-1, 0xFA2C00);
+				mlx_pixel_put(f->mlx_ptr, f->win_ptr, x_wall - 1, y_wall + 1, 0xFA2C00);
+				mlx_pixel_put(f->mlx_ptr, f->win_ptr, x_wall + 1, y_wall - 1, 0xFA2C00);
+			}
+			x++;
+			x_wall += 15;
+		}
+		x = 0;
+		x_wall = 15;
+		
+		y++;
+		y_wall += 15;
+	}
+}
+
+void raycasting (t_cub3d *f)
+{
+
+
+	ft_putstr("hola");
 }
 
 int		main(void)
@@ -46,29 +104,16 @@ int		main(void)
 
 	f = malloc(sizeof(t_cub3d));
 
-	//x and y start position
-	f->posX = 22;
-	f->posY = 12;  
-
-	//initial direction vector
-	f->dirX = -1;
-	f->dirY = 0;
-
-	 //the 2d raycaster version of camera plane
-  	f->planeX = 0;
-	f->planeY = 0.66;
-
-  	f->time = 0; //time of current frame
-	f->oldTime = 0; //time of previous frame
-
+	f->x_position = 30;
+	f->y_position = 30;
 	
 	f->mlx_ptr = mlx_init();
-	f->win_ptr = mlx_new_window(f->mlx_ptr, 800, 800, "mx 42");
-		
+	f->win_ptr = mlx_new_window(f->mlx_ptr, screenWidth, screenHeight, "mx 42");
+
 	//mlx_pixel_put(f->mlx_ptr, f->win_ptr, f->x_position, 200, 0xFFD2C8);
+	drawMap(&(*f));
+	raycasting(&(*f));
 	mlx_key_hook(f->win_ptr, deal_key, &(*f));
-	while (!done())
-	{
-		mlx_loop(f->mlx_ptr);
-	}
+	
+	mlx_loop(f->mlx_ptr);
 }
