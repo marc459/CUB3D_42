8f3f27dd724142b26cb9d72a81e0c2992d2e7941
marcos <santosserranomarcos@gmail.com>
2020-07-12 20:52:26 +0200

    main.c changes

--- Test_cloned_cub3d/raycasterSDL_to_raycasterMLX/src/main.c
+++ Test_cloned_cub3d/raycasterSDL_to_raycasterMLX/src/main.c
@@ -82,16 +82,94 @@ int		handle_events(int key,t_raycaster *rc){
 
 }
 
+void          initial_calc(t_raycaster *rc, int x)
+{
+  double      camera_x;
+
+  camera_x = 2 * x / (double)(WIN_X) - 1;
+  rc->ray_dir_x = rc->player_dir_x + rc->player_plane_x * camera_x;
+  rc->ray_dir_y = rc->player_dir_y + rc->player_plane_y * camera_x;
+  rc->map_x = (int)(rc->player_pos_x);
+  rc->map_y = (int)(rc->player_pos_y);
+  rc->delta_dist_x = sqrt(1 + pow(rc->ray_dir_y, 2) / pow(rc->ray_dir_x, 2));
+  rc->delta_dist_y = sqrt(1 + pow(rc->ray_dir_x, 2) / pow(rc->ray_dir_y, 2));
+  if (rc->ray_dir_x < 0)
+  {
+    rc->step_x = -1;
+    rc->side_dist_x = (rc->player_pos_x - rc->map_x) * rc->delta_dist_x;
+  }
+  else
+  {
+    rc->step_x = 1;
+    rc->side_dist_x = (rc->map_x + 1.0 - rc->player_pos_x) * rc->delta_dist_x;
+  }
+  if (rc->ray_dir_y < 0)
+  {
+    rc->step_y = -1;
+    rc->side_dist_y = (rc->player_pos_y - rc->map_y) * rc->delta_dist_y;
+  }
+  else
+  {
+    rc->step_y = 1;
+    rc->side_dist_y = (rc->map_y + 1.0 - rc->player_pos_y) * rc->delta_dist_y;
+  }
+}
+
+void          perform_dda(t_raycaster *rc)
+{
+  int         hit;
+
+  hit = 0;
+  while (hit == 0)
+  {
+    if (rc->side_dist_x < rc->side_dist_y)
+    {
+      rc->side_dist_x += rc->delta_dist_x;
+      rc->map_x += rc->step_x;
+      rc->side = 0;
+    }
+    else
+    {
+      rc->side_dist_y += rc->delta_dist_y;
+      rc->map_y += rc->step_y;
+      rc->side = 1;
+    }
+    if (worldMap[rc->map_x][rc->map_y] > 0)
+      hit = 1;
+  }
+}
+
+void          calc_wall_height(t_raycaster *rc)
+{
+  int         line_height;
+
+  if (rc->side == 0)
+    rc->perp_wall_dist = (rc->map_x - rc->player_pos_x + (1 - rc->step_x) / 2) / rc->ray_dir_x;
+  else
+    rc->perp_wall_dist = (rc->map_y - rc->player_pos_y + (1 - rc->step_y) / 2) / rc->ray_dir_y;
+  line_height = (int)(WIN_Y / rc->perp_wall_dist);
+  rc->draw_start = -line_height / 2 + WIN_Y / 2;
+  if (rc->draw_start < 0)
+    rc->draw_start = 0;
+  rc->draw_end = line_height / 2 + WIN_Y / 2;
+  if (rc->draw_end >= WIN_Y)
+    rc->draw_end = WIN_Y - 1;
+}
+
 //principal loop
 int raycasting(int key, t_raycaster *rc)
 {
-	/*for(int x = 0; x < WIN_X; x++)
+	int x;
+
+	x = 0;
+	while(x < WIN_X)
     {
-      initial_calc(&(*rc), x);
-      perform_dda(&(*rc));
-      calc_wall_height(&(*rc));
-      draw_vert_line(sdl, &(*rc), x);
-    }*/
+		initial_calc(rc, x);
+		perform_dda(rc);
+    	calc_wall_height(rc);
+      	draw_vert_line(sdl, rc, x);
+	  	x++;
+    }
 
 	refresh_screen(rc);
 	handle_events(key,rc);
@@ -103,7 +181,6 @@ int		main(void)
 	t_raycaster		*rc;
 
 	rc = malloc(sizeof(t_raycaster));
-
 	if (init(rc) != 0)
 		return (-1);
 	
