548f14897cef363aeb822d684b4655047249dbc4
marcos <santosserranomarcos@gmail.com>
2020-08-27 12:39:58 +0200

    check mac errors

--- Cub3dmerged/src/valid_map.c
+++ Cub3dmerged/src/valid_map.c
@@ -134,19 +134,28 @@ int		valid_map(char *line, validmap_t *map, archparams_t *arch)
 	if (map->m_top == 0)
 	{
 		if (!check_top_map(line, map, i, count))
+		{
 			printf("err1");
+			return (0);
+		}	
 	}
 	else if (map->m_bot == 0)
 	{
 		if (map->colum_spaces[0] > 0)
 			i = -1;
 		if (!sourrounding_walls(line, map, i, count))
+		{
 			printf("err2");
+			return (0);
+		}	
 		i = 0;
 		init_map_checking_params(map);
 		if (!check_map_bowels(line, map, i, count)
 		|| !check_bot_map(line, map, i, count))
+		{
 			printf("err3");
+			return (0);
+		}	
 	}
 	save_map(line, map, arch, i);
 	map->m_line++;
