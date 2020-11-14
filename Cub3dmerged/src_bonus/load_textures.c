277020857ad75205e1e03ad1b8c1a356ef043f05
marcos <santosserranomarcos@gmail.com>
2020-11-15 00:34:17 +0100

    inferno textures added

--- Cub3dmerged/src_bonus/load_textures.c
+++ Cub3dmerged/src_bonus/load_textures.c
@@ -18,11 +18,11 @@ void	load_sounds()
 
 void	load_textures_2(t_raycaster *rc)
 {
-	rc->tex[6].img = mlx_xpm_file_to_image(rc->mlx_ptr, "sprites/barrel.xpm",
+	rc->tex[6].img = mlx_xpm_file_to_image(rc->mlx_ptr, "textures/inferno/sprite.xpm",
 			&rc->tex_width, &rc->tex_height);
 	rc->tex[6].data = mlx_get_data_addr(rc->tex[6].img, &rc->tex[6].bpp,
 			&rc->tex[6].size_l, &rc->tex[6].endian);
-	rc->tex[7].img = mlx_xpm_file_to_image(rc->mlx_ptr, "sprites/light.xpm",
+	rc->tex[7].img = mlx_xpm_file_to_image(rc->mlx_ptr, "textures/inferno/cielo.xpm",
 			&rc->tex_width, &rc->tex_height);
 	rc->tex[7].data = mlx_get_data_addr(rc->tex[7].img, &rc->tex[7].bpp,
 			&rc->tex[7].size_l, &rc->tex[7].endian);
@@ -42,11 +42,11 @@ void	load_textures_2(t_raycaster *rc)
 
 void	load_textures(t_raycaster *rc)
 {
-	rc->tex[1].img = mlx_xpm_file_to_image(rc->mlx_ptr, "textures/stone.xpm",
+	rc->tex[1].img = mlx_xpm_file_to_image(rc->mlx_ptr, "textures/inferno/pared1.xpm",
 			&rc->tex_width, &rc->tex_height);
 	rc->tex[1].data = mlx_get_data_addr(rc->tex[1].img, &rc->tex[1].bpp,
 			&rc->tex[1].size_l, &rc->tex[1].endian);
-	rc->tex[2].img = mlx_xpm_file_to_image(rc->mlx_ptr, "textures/mossy.xpm",
+	rc->tex[2].img = mlx_xpm_file_to_image(rc->mlx_ptr, "textures/inferno/pared2.xpm",
 			&rc->tex_width, &rc->tex_height);
 	rc->tex[2].data = mlx_get_data_addr(rc->tex[2].img, &rc->tex[2].bpp,
 			&rc->tex[2].size_l, &rc->tex[2].endian);
@@ -54,7 +54,7 @@ void	load_textures(t_raycaster *rc)
 			&rc->tex_width, &rc->tex_height);
 	rc->tex[3].data = mlx_get_data_addr(rc->tex[3].img, &rc->tex[3].bpp,
 			&rc->tex[3].size_l, &rc->tex[3].endian);
-	rc->tex[4].img = mlx_xpm_file_to_image(rc->mlx_ptr, "textures/wood.xpm",
+	rc->tex[4].img = mlx_xpm_file_to_image(rc->mlx_ptr, "textures/inferno/suelo.xpm",
 			&rc->tex_width, &rc->tex_height);
 	rc->tex[4].data = mlx_get_data_addr(rc->tex[4].img, &rc->tex[4].bpp,
 			&rc->tex[4].size_l, &rc->tex[4].endian);
